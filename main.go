92c097b08283c94b2550216e3662236288b9a7ba
Trend20 <enockomondi305@gmail.com>
2024-09-04 17:03:14 +0300

    adding the book model

--- main.go
+++ main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Simple RESTful API built using  Gin and GORM")
 
-	//	initialize a server
+	//	initialize a router
 	router := gin.Default()
 
 	//simple test route
 	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
